a8f2565b583d0a15aeb61f269cb193f79022076e
179640 <179640@edu.p.lodz.pl>
2016-11-22 12:05:51 +0100

    init commit

---
+++ src/main/java/pl/lodz/p/edu/ppkwu/187824and179640/UiforspringApplication.java
@@ -0,0 +1,12 @@
+package pl.lodz.p.edu.ppkwu.187824and179640;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UiforspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UiforspringApplication.class, args);
+	}
+}
---
+++ src/test/java/pl/lodz/p/edu/ppkwu/187824and179640/UiforspringApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.lodz.p.edu.ppkwu.187824and179640;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UiforspringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
